9f7d2709148ee17bfe272f23ee3e85d717902784
XperiaZed <18zgriffin@mlmc.vic.edu.au>
2017-11-21 12:38:46 +1100

    Added input entries and difference between ',' and '+' in strings

--- Test Python.py
+++ Test Python.py
@@ -1,1 +1,10 @@
 print("Hello World")
+
+name = input("You?: ")
+age = int(input("Age: "))
+
+print("Hello", name, age)
+print("Hello " + name + " " + str(age))
+#the comma simply outputs the result
+#the plus tries to output the variables as they are
+#therefore we must convert the integer to a string outselves
