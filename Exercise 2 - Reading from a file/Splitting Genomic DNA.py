9e03dabc6dd9d906ca9326b9cf47aad4ddee0279
XperiaZed <18zgriffin@mlmc.vic.edu.au>
2017-11-23 12:36:39 +1100

    Completed exercise 2 adding files Exons.txt, Intron.txt, genomic_dna.txt and Splitting Genomes. All while completing file writing code

---
+++ Exercise 2 - Reading from a file/Splitting Genomic DNA.py
@@ -0,0 +1,27 @@
+#opens all needed files
+file = open("genomic_dna.txt")
+intronFile = open("Intron.txt", "w")
+exonFile = open("Exons.txt", "w")
+
+#assigns main file data to a variable
+dnaSequence = file.read()
+
+#calcualate the length of the sequence
+lengthSequence = len(dnaSequence)
+
+#finds both exons and prints them
+exon1 = dnaSequence[0:63]
+exon2 = dnaSequence[91:lengthSequence]
+print("Exon 1 is", exon1)
+print("Exon 2 is", exon2)
+
+#finds the intron and then prints it
+intron = dnaSequence[64:90]
+print("The intron is", intron)
+
+#writes exon and introns to their appropriate files
+intronFile.write(intron)
+exonFile.write(exon1)
+exonFile.write("\n")
+exonFile.write(exon2)
+
