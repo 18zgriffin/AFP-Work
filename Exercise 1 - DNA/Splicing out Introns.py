c19ffe4724f7c757b09896da5999b7501869ad07
XperiaZed <18zgriffin@mlmc.vic.edu.au>
2017-11-23 12:12:54 +1100

    Completed the second part of splicing out introns

--- Exercise 1 - DNA/Splicing out Introns.py
+++ Exercise 1 - DNA/Splicing out Introns.py
@@ -1,9 +1,25 @@
 dnaSequence = "ATCGATCGATCGATCGACTGACTAGTCATAGCTATGCATGTAGCTACTCGATCGATCGATCGATCGATCGATCGATCGATCGATCATGCTATCATCGATCGATATCGATGCATCGACTACTAT"
 
-length = len(dnaSequence)
+lengthSequence = len(dnaSequence)
 
+#finds both exons and prints them
 exon1 = dnaSequence[0:63]
-exon2 = dnaSequence[91:length]
+exon2 = dnaSequence[91:lengthSequence]
+print("Exon 1 is", exon1)
+print("Exon 2 is", exon2)
 
-print(exon1)
-print(exon2)
+#finds the length each exon
+lengthExon1 = len(exon1)
+lengthExon2 = len(exon2)
+print("The length of Exon 1 is", lengthExon1, "and the length of Exon 2 is", lengthExon2)
+
+#finds the percentage that each exon is of the total
+lengthExon1percent = 100/lengthSequence*lengthExon1
+lengthExon2percent = 100/lengthSequence*lengthExon2
+
+#adds exon percentages together and prints is unrounded
+exonPercentage = lengthExon1percent+lengthExon2percent
+print("The unrouned exon percentage is", exonPercentage)
+
+#prints the exon percentage with 2 decimals points
+print("The rounded exon percentage is", "%.2f" % exonPercentage)
