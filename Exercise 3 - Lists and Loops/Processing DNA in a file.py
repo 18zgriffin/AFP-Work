23bcd8d82acd23390bdd58dd52a54fe31e2a5f09
XperiaZed <18zgriffin@mlmc.vic.edu.au>
2017-11-27 17:53:53 +1100

    Added the close file at the end

--- Exercise 3 - Lists and Loops/Processing DNA in a file.py
+++ Exercise 3 - Lists and Loops/Processing DNA in a file.py
@@ -5,10 +5,8 @@ trimmedFile = open("trimmed_sequences.txt", "w")
 for line in mainFile:
     lineLength = len(line)
     trimmedLine = line[14:lineLength]
-
     print(trimmedLine)
     print("The trimmed line is", len(trimmedLine), "values long")
-
     trimmedFile.write(trimmedLine)
     trimmedFile.write("\n")
 
